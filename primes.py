066f41199f981fe67364f1361a81702ffd172660
Yohann Pirbay <pirbay.yohann@gmail.com>
2023-10-09 19:24:22 +0100

    Copy code from previous list of prime

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    while len(list) < number_of_primes:
+        if (isPrime(count)):
+            list.append(count)
+        count += 1
     return list
+
+def isPrime(x):
+    count = 0
+    for i in range(1, x//2 + 1):
+        if x % i == 0:
+            count += 1
+    if (count == 1):
+        return True
+    else:
+        return False
